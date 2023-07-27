340b91e7197c3226db0a99011e322ad9b787c5e3
datata <dani@danitarazona.dev>
2023-07-27 16:54:54 +0200

    feat: react router dom with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Login from './pages/Login/Login'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Login/>
+      <Routes>
+        <Route path='*' element={<><h1>Home</h1></>}/>
+        <Route path='/home' element={<><h1>Home</h1></>}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
     </>
   )
 }
