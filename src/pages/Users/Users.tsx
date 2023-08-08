4f708b5cfe335e047d4a7371f830b09f1b5637e2
datata <dani@danitarazona.dev>
2023-08-08 14:18:46 +0200

    feat: users pagination

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import UserList from "../../components/UsersList/UserList";
 import Toaster from "../../components/Toaster/Toaster";
 
@@ -17,17 +17,19 @@ const Users = () => {
   const [limitUsers, setLimitUsers] = useState<number>(10)
 	const [toaster, setToaster] = useState(false)
 	const [toasterError, setToasterError] = useState(false)
+  const [skip, setSkip] = useState<number>(0)
 
   useEffect(()=>{
     setIsLoading(true)
 
-    fetch(`https://dummyjson.com/users?limit=${limitUsers}`)
+    fetch(`https://dummyjson.com/users?limit=${limitUsers}&skip=${skip}`)
       .then((res) => {
         if(!res.ok) throw new Error('Retrieving users error')
         return res.json()
       })
-      .then((res) => {
-        setUsers(res.users)
+      .then((res) => {       
+        setUsers(users.concat(res.users))
+        
         setToaster(true);
         setTimeout(() => {
           setToaster(false)
@@ -40,24 +42,30 @@ const Users = () => {
           setToasterError(false)
         }, 2000);
       })
-      .finally(() =>  setIsLoading(false))    
-  }, [limitUsers])
+      .finally(() =>  {
+        setIsLoading(false) 
+      })    
+  }, [limitUsers, skip])
 
   return (
     <>
-    {toaster ? <Toaster message={'Success!'} /> : ""}
-    {toasterError ? <Toaster message={"Error"} type={'error'}/> : ""}
-    { isLoading 
-      ? <div>Loading users...</div>
-      : <>
-          <button><Link to="/profile"> Profile </Link></button>
-          <button onClick={() => {
-            setLimitUsers(limitUsers + 10)
-          }}> 10 more Users </button>
-          <button onClick={() => setLimitUsers(100)}> All Users </button>
-          <UserList data={users}/>
-        </>
-    }
+      {toaster ? <Toaster message={'Success!'} /> : ""}
+      {toasterError ? <Toaster message={"Error"} type={'error'}/> : ""}
+      <button onClick={() => setLimitUsers(100)}> All Users </button>
+      { users.length > 0 && <>
+            {/* <button><Link to="/profile"> Profile </Link></button> */}
+            {/* <button 
+              onClick={() => {
+                setLimitUsers(limitUsers + 10)
+              }}
+              > 
+              10 more Users 
+            </button> */}
+            <UserList data={users}/>
+          </>
+      }
+      { limitUsers !== 100 && <button onClick={() => setSkip(skip + 10)}> Next page </button>}
+      { isLoading && <div>Loading users...</div>}
     </>
   )
 }
